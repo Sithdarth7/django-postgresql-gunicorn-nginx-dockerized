040a1f58b9b364ec8f0a6710f7e1dd8decac5c5c
Duke <Duke@DESKTOP-G1RR7VB>
2024-08-07 12:23:44 +0700

    ..

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,50 +1,174 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "main_a" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
+}
+resource "aws_subnet" "main_b" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-west-2b"
+}
+
+resource "aws_security_group" "ecs" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow access from the internet
   }
 
-  required_version = ">= 0.13.4"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
-provider "aws" {
-  access_key                  = "test"
-  secret_key                  = "test"
-  region                      = "us-east-1"
-  s3_force_path_style         = false
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
 
-  endpoints {
-    ecs            = "http://localhost:4566"
-    apigateway     = "http://localhost:4566"
-    apigatewayv2   = "http://localhost:4566"
-    cloudformation = "http://localhost:4566"
-    cloudwatch     = "http://localhost:4566"
-    dynamodb       = "http://localhost:4566"
-    ec2            = "http://localhost:4566"
-    es             = "http://localhost:4566"
-    elasticache    = "http://localhost:4566"
-    firehose       = "http://localhost:4566"
-    iam            = "http://localhost:4566"
-    kinesis        = "http://localhost:4566"
-    lambda         = "http://localhost:4566"
-    rds            = "http://localhost:4566"
-    redshift       = "http://localhost:4566"
-    route53        = "http://localhost:4566"
-    s3             = "http://s3.localhost.localstack.cloud:4566"
-    secretsmanager = "http://localhost:4566"
-    ses            = "http://localhost:4566"
-    sns            = "http://localhost:4566"
-    sqs            = "http://localhost:4566"
-    ssm            = "http://localhost:4566"
-    stepfunctions  = "http://localhost:4566"
-    sts            = "http://localhost:4566"
+# Security group for RDS database
+resource "aws_security_group" "rds" {
+  vpc_id = aws_vpc.main.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow inbound access from the ECS security group
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    security_groups = [aws_security_group.ecs.id]  # Allow traffic from ECS security group
   }
 }
 
 resource "aws_ecs_cluster" "main" {
-  name = "my-ecs-cluster"
+  name = "my-cluster"
+}
+
+resource "aws_ecs_task_definition" "web" {
+  family                   = "my-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = jsonencode([{
+    name      = "web"
+    image     = "022499043855.dkr.ecr.us-east-1.amazonaws.com/django-web:prod-latest"
+    cpu       = 256
+    memory    = 512
+    essential = true
+
+    portMappings = [
+      {
+        containerPort = 8000
+        hostPort      = 8000
+        protocol      = "tcp"
+      }
+    ]
+
+    environment = [
+      {
+        name  = "DJANGO_SETTINGS_MODULE"
+        value = "myapp.settings.production"
+      }
+    ]
+  }])
+
+  execution_role_arn = aws_iam_role.ecs_execution_role.arn
+  task_role_arn      = aws_iam_role.ecs_task_role.arn
+}
+
+resource "aws_ecs_service" "web" {
+  name            = "web-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.web.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = [aws_subnet.main_a.id]
+    security_groups  = [aws_security_group.ecs.id]
+    assign_public_ip = true
+  }
+}
+
+resource "aws_db_instance" "db" {
+  identifier        = "db"
+  instance_class    = "db.t3.micro"
+  engine            = "postgres"
+  engine_version    = "12.19"
+  allocated_storage = 20
+  username          = "hello_django"
+  password          = "hello_django"
+  name           = "hello_django_prod"
+  parameter_group_name = "default.postgres12"
+  publicly_accessible = false
+  vpc_security_group_ids = [aws_security_group.rds.id]
+  db_subnet_group_name = aws_db_subnet_group.main.id
+  tags = {
+    Name = "db"
+  }
+}
+
+resource "aws_db_subnet_group" "main" {
+  name       = "my-db-subnet-group"
+  subnet_ids = [aws_subnet.main_a.id, aws_subnet.main_b.id]
+}
+
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "ecs_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  role     = aws_iam_role.ecs_execution_role.name
+}
+
+resource "aws_iam_role" "ecs_task_role" {
+  name = "ecs_task_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_ecr_repository" "web" {
+  name = "django-web"
 }
